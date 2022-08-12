92499b9390ca291cada574b8cdc974c12aea6308
Shivaprasad431 <111137636+Shivaprasad431@users.noreply.github.com>
2022-08-12 21:01:32 +0530

    Add files via upload

---
+++ pythonbasics.py
@@ -0,0 +1,13 @@
+#this is about the some basics of the python
+print("hi\n")
+print('my name is shiva prasad')
+print("Iam more intrested in doing this fullstack develoment course")
+print("I hope ill learn and get more knowledge in this course")
+#adding of two numbers
+print(20+30)
+#subraction of two numbers
+print(30-20)
+#multiplication of two numbers
+print(30*20)
+#division of two numbers
+print(30/20)
