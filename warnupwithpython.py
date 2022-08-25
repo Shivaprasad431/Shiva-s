7362dd304e95db83ff4612137aff5defc41f0676
Shivaprasad431 <111137636+Shivaprasad431@users.noreply.github.com>
2022-08-26 00:24:29 +0530

    Update warnupwithpython.py

--- warnupwithpython.py
+++ warnupwithpython.py
@@ -1,1 +1,13 @@
-print("hello iam presently doing at i neuron web development course")
+import sys
+print(sys.version)
+print("Hello Python")
+print("MySirG")
+print("Teacher's Day")
+a="shiva prasad"
+print(a)
+name, age, qualification, years of experience="Shiva Prasad",21,"BTECH","3 years"
+print(name, age , qualification, years of experience)
+a,b,c=10,20,30
+print(a)
+print(b)
+print(c)
