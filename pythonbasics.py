799d6f6c41c22d2ebae488643360924709a938cf
Shivaprasad431 <111137636+Shivaprasad431@users.noreply.github.com>
2022-08-26 09:59:00 +0530

    Update pythonbasics.py

--- pythonbasics.py
+++ pythonbasics.py
@@ -1,13 +1,40 @@
-#this is about the some basics of the python
-print("hi\n")
-print('my name is shiva prasad')
-print("Iam more intrested in doing this fullstack develoment course")
-print("I hope ill learn and get more knowledge in this course")
-#adding of two numbers
-print(20+30)
-#subraction of two numbers
-print(30-20)
-#multiplication of two numbers
-print(30*20)
-#division of two numbers
-print(30/20)
+#this is about the some basics of the python,this is how we write single line comments
+print("Learning python")
+'''
+this is how we write multi line comments
+we use three single or doubled quotes at the start and ending points of the sentences
+'''
+import a1
+
+
+from datetime import datetime
+dt=datetime.today();
+print(dt)
+print(a1.x)
+a,b,c,d=10,20,30,40
+print(a)
+print(b)
+print(c)
+print(d)
+p,q,r,s,t=35, True, “MySirG”,5.46, 3+4j
+print(p,type(p))
+print(q,type(q))
+print(r,type(r))
+print(s,type(s))
+print(t,type(t))
+z=40
+x=40
+print(id(z))
+print(id(x))
+print(p,type(p),id(p))
+print(q,type(q),id(q))
+print(r,type(r),id(p))
+print(s,type(s),id(p))
+print(t,type(t),id(p))
+help("keywords")
+
+"""None True False are used as variables"""
+
+
+
+
