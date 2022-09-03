273fc5f7c01a4e42103ba22d296476848e835555
Shivaprasad431 <111137636+Shivaprasad431@users.noreply.github.com>
2022-09-04 00:25:50 +0530

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,26 @@
+x=input()
+a=x[:-1]
+print(a)
+y=input()
+b=y[-1]
+print(b)
+x,y=y,x
+x=int(input())
+y=int(input())
+print(x**y)
+a=input("Enter a three digit number")
+print(a[0])
+print(a[1])
+print(a[-1])
+a="Shiva Manju"
+if Manju in a:
+    print("YES")
+elif chintu not in a:
+    print("NO")
+a=104
+b=104
+if a is b:
+    print("YES")
+
+
+
