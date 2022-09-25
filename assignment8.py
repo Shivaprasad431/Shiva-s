ec741a4c431131aab2e53c7b5174507077763325
Shiva Prasad Koyyada <111137636+Shivaprasad431@users.noreply.github.com>
2022-09-25 09:39:19 +0530

    Add files via upload

---
+++ assignment8.py
@@ -0,0 +1,46 @@
+i=1
+while i<=5:
+    print("MySirG")
+    i+=1
+j=1
+while j<=10:
+    print(j)
+    j+=1
+k=10
+while k>=1:
+    print(k)
+    k-=1
+a=1
+while a<=20:
+    if a%2!=0:
+        print(a)
+    a+=1
+c=20
+while c>0:
+    if c%2!=0:
+        print(c)
+    c-=1
+a=1
+while a<=20:
+    if a%2==0:
+        print(a)
+    a+=1
+c=20
+while c>0:
+    if c%2==0:
+        print(c)
+    c-=1
+a=1
+while a<=10:
+    if a%2==0:
+        print(a**2)
+    a+=1
+a=1
+while a<=10:
+    if a%2==0:
+        print(a**3)
+    a+=1
+a=1
+while a<=5:
+  print(a*10)
+  a+=1
