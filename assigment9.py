76c93c7f03592061ae0e3622cefc78552b0b809a
Shiva Prasad Koyyada <111137636+Shivaprasad431@users.noreply.github.com>
2022-09-25 10:09:13 +0530

    Add files via upload

---
+++ assigment9.py
@@ -0,0 +1,54 @@
+n=int(input("give how many times you eant to print the term\n"))
+i=1
+while i<=n:
+    print("MySirG")
+    i+=1
+n=int(input("give the the range of natural numbers\n"))
+i=1
+while i<=n:
+    print(i)
+    i+=1
+n=int(input("give the the range of natural numbers\n"))
+i=n
+while i>=1:
+    print(i)
+    i-=1
+n=int(input("give the the range of natural numbers\n"))
+i=1
+while i<=n:
+    if i%2==0:
+      print(i)
+    i+=1
+n=int(input("give the the range of natural numbers\n"))
+i=n
+while i>=1:
+    if i%2==0:
+     print(i)
+    i-=1
+n=int(input("give the the range of natural numbers\n"))
+i=1
+while i<=n:
+    if i%2!=0:
+      print(i)
+    i+=1
+n=int(input("give the the range of natural numbers\n"))
+i=n
+while i>=1:
+    if i%2!=0:
+     print(i)
+    i-=1  
+n=int(input("give the the range of natural numbers\n"))
+i=1
+while i<=n:
+    print(i**2)
+    i+=1
+n=int(input("give the the range of natural numbers\n"))
+i=1
+while i<=n:
+    print(i**3)
+    i+=1
+n=int(input("give the the range of natural numbers\n"))
+i=1
+while i<=n:
+    print(i*10)
+    i+=1
