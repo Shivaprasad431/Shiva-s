d25c36a45c529575191d540eff038ddc408c6540
Shiva Prasad Koyyada <111137636+Shivaprasad431@users.noreply.github.com>
2022-11-20 09:40:34 +0530

    Add files via upload

---
+++ assignment15.py
@@ -0,0 +1,28 @@
+print('hi I am VIP')
+print("hi I am VIP")
+print('''hi I am VIP''')
+st='iNeuron'
+print(st[5])
+st='iNeuron'
+print(st[2:5])
+a="Learning"
+b="Python"
+print(a+b)
+st="Shivaprasad"
+print(len(st))
+st='iNeuron'
+print(st[::-1])
+st='iNeuron'
+st1='Neuron'
+if st1 in st:
+    print("Yes Present")
+else:
+    print("False")
+st="1234shdfjg"
+print(st.isdigit())
+st="1234shdfjg"
+print(st.isalpha())
+n=123445
+st=str(n)
+print(st)
+print(type(st))
